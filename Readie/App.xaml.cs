805aaa4e2454d323e603fb917277235aab679835
Dursun Zahid Korkmaz <67268132+Zerohidz@users.noreply.github.com>
2022-10-04 20:21:50 +0300

    Add project files.

---
+++ Readie/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Readie;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Readie/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Readie;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
