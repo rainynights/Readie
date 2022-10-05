19f98995110b76b93e68714e8ba22fefeb120b20
Ali Hakan Kurt <kurtalihakan@outlook.com>
2022-10-05 19:05:22 +0300

    Edited colors and styles

--- Readie/AppShell.xaml.cs
+++ Readie/AppShell.xaml.cs
@@ -2,8 +2,24 @@
 
 public partial class AppShell : Shell
 {
+	public Command ChangeThemeCommand { get; }
+	
+	public ImageSource Moon { get; }
+	public ImageSource Sun { get; }
+
 	public AppShell()
 	{
-		InitializeComponent();
+		ChangeThemeCommand = new Command(ChangeTheme);
+		Moon = "moon.png";
+		Sun = "sun.png";
+        InitializeComponent();
+		BindingContext = this;
 	}
+
+	private void ChangeTheme()
+	{
+		Application.Current.UserAppTheme = Application.Current.RequestedTheme == AppTheme.Light
+			? AppTheme.Dark
+			: AppTheme.Light;
+    }
 }
