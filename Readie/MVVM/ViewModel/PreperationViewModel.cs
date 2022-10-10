c0f68c04e2d7c06e715419fe16f25cf6a74a11b0
Ali Hakan Kurt <kurtalihakan@outlook.com>
2022-10-10 21:08:27 +0300

    NumericUpDown Commit 2

--- Readie/MVVM/ViewModel/PreperationViewModel.cs
+++ Readie/MVVM/ViewModel/PreperationViewModel.cs
@@ -5,11 +5,12 @@ namespace Readie.MVVM.ViewModel;
 [QueryProperty(nameof(SelectedText), nameof(SelectedText))]
 public class PreperationViewModel : ViewModelBase
 {
-    private Text _selectedText;
+    public const int Increment = 10;
+    public const int MinWordsPerMinute = 30;
+    public const int MaxWordsPerMinute = 320;
 
-    public Command SelectTextCommand { get; }
-    public Command SelectFileCommand { get; }
-    public Command ReadCommand { get; }
+    private Text _selectedText;
+    private int _wordsPerMinute;
 
     public string SelectedTextPreview => _selectedText?.GetPreview() ?? Text.DefaultPreview;
     public Text SelectedText
@@ -23,13 +24,30 @@ public class PreperationViewModel : ViewModelBase
         }
     }
 
-    public int WordsPerMinute { get; set; } = 30;
+    public int WordsPerMinute
+    {
+        get => _wordsPerMinute;
+        set => SetProperty(ref _wordsPerMinute, value);
+    }
+
+    public int[] WordsToShow { get; } = new[] { 1, 2, 3 };
+    public int SelectedWordsToShow { get; set; }
+
+    public Command SelectTextCommand { get; }
+    public Command SelectFileCommand { get; }
+    public Command ReadCommand { get; }
+    public Command DecreaseWPMCommand { get; }
+    public Command IncreaseWPMCommand { get; }
 
     public PreperationViewModel()
     {
         SelectTextCommand = new Command(SelectText);
         SelectFileCommand = new Command(SelectFile);
         ReadCommand = new Command(Read);
+
+        _wordsPerMinute = MinWordsPerMinute;
+        DecreaseWPMCommand = new Command(() => WordsPerMinute = Math.Clamp(WordsPerMinute - Increment, MinWordsPerMinute, MaxWordsPerMinute));
+        IncreaseWPMCommand = new Command(() => WordsPerMinute = Math.Clamp(WordsPerMinute + Increment, MinWordsPerMinute, MaxWordsPerMinute));
     }
 
     private async void SelectText()
