f03b1854dd96a442971fe5080d504b26bba525b5
b2210356020 <b2210356020@cs.hacettepe.edu.tr>
2022-10-21 12:08:39 +0300

    Light Theme Silindi

--- Readie/AppShell.xaml.cs
+++ Readie/AppShell.xaml.cs
@@ -2,19 +2,8 @@
 
 public partial class AppShell : Shell
 {
-	public Command ChangeThemeCommand { get; }
-	public string ThemeIcon => (Application.Current.RequestedTheme == AppTheme.Light) ? "sun.png" : "moon.png";
-
 	public AppShell()
 	{
-		ChangeThemeCommand = new Command(ChangeTheme);
         InitializeComponent();
-		BindingContext = this;
 	}
-
-	private void ChangeTheme()
-	{
-		Application.Current.UserAppTheme = (Application.Current.RequestedTheme == AppTheme.Light) ? AppTheme.Dark : AppTheme.Light;
-		OnPropertyChanged(nameof(ThemeIcon));
-    }
 }
